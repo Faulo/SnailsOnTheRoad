bc7ba344bcf9faf12062a3a74ba825598e99f760
Daniel Schulz <faulolio@gmail.com>
2024-11-16 00:46:26 +0100

    fix: namespace

--- Assets/Modules/Player/Runtime/AssemblyInfo.cs
+++ Assets/Modules/Player/Runtime/AssemblyInfo.cs
@@ -6,8 +6,8 @@ using SotR.Player;
 
 namespace SotR.Player {
     static class AssemblyInfo {
-        internal const string NAMESPACE_RUNTIME = "SitS.Player";
-        internal const string NAMESPACE_EDITOR = "SitS.Player.Editor";
-        internal const string NAMESPACE_TESTS = "SitS.Player.Tests";
+        internal const string NAMESPACE_RUNTIME = "SotR.Player";
+        internal const string NAMESPACE_EDITOR = "SotR.Player.Editor";
+        internal const string NAMESPACE_TESTS = "SotR.Player.Tests";
     }
 }
