de76cca0ed7320414993ee0bef0c4fbc29e36294
Daniel Schulz <faulolio@gmail.com>
2024-11-17 12:33:03 +0100

    feat: level loader

---
+++ Assets/Modules/Game/Runtime/LevelLoader.cs
@@ -0,0 +1,37 @@
+﻿using Slothsoft.UnityExtensions;
+using UnityEngine;
+
+namespace SotR.Game {
+    [ExecuteAlways]
+    sealed class LevelLoader : MonoBehaviour {
+        [SerializeField, Expandable]
+        GameSettings settings;
+        [SerializeField]
+        Transform levelPivot;
+
+        GameObject loadedLevelPrefab;
+        GameObject loadedLevelInstance;
+
+        void Update() {
+            if (!settings || !levelPivot) {
+                return;
+            }
+
+            if (loadedLevelPrefab != settings.currentLevelPrefab) {
+                loadedLevelPrefab = settings.currentLevelPrefab;
+                levelPivot.Clear();
+
+                if (loadedLevelPrefab) {
+                    if (Application.isPlaying) {
+                        loadedLevelInstance = Instantiate(loadedLevelPrefab, levelPivot);
+                    } else {
+#if UNITY_EDITOR
+                        loadedLevelInstance = UnityEditor.PrefabUtility.InstantiatePrefab(loadedLevelPrefab, levelPivot) as GameObject;
+                        loadedLevelInstance.hideFlags = HideFlags.DontSave;
+#endif
+                    }
+                }
+            }
+        }
+    }
+}
